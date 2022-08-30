b2ae581f0ea255f8e259abf21d05a8d44938061c
julien deantoni <julien.deantoni@inria.fr>
2022-08-30 11:25:25 +0200

    update helper

--- content/helperIIW.py
+++ content/helperIIW.py
@@ -3,8 +3,26 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import os
 
+class Pixel:
+    def __init__(self, theR = 0, theG = 0, theB = 0):
+        self.r=theR
+        self.g=theG
+        self.b=theB
+    def __repr__(self):
+        return f'Pixel({self.r},{self.g},{self.b})'
 
-def showImage(path)
+
+class Image:
+    def __init__(self, l, w, ps):
+        self.length=l
+        self.width=w
+        self.pixels=ps
+    def __repr__(self):
+        return f'Image(\n  {self.length}x{self.width}\n  {self.pixels}\n)'
+
+
+
+def showImage(path):
     img = cv2.imread(path)
     # Remember, opencv by default reads images in BGR rather than RGB
     # So we fix that by the following
@@ -14,3 +32,40 @@ def showImage(path)
     img2.show()
     os.remove(".temp.png") 
     
+def loadImage(path):
+    allPixels = []
+    f = open(path, "r")
+    allLines= f.readlines()
+    usefullLines = []
+    for line in allLines:
+        if line.startswith('#') or len(line)==0:
+            None
+        else:
+            usefullLines.append(line)
+
+    usefullLines = usefullLines[1:]
+    dimensionsAndPixels=[]
+
+    for line in usefullLines:
+        for w in line.split():
+            dimensionsAndPixels.append(w)
+
+
+    length=dimensionsAndPixels[0]
+    width=dimensionsAndPixels[1]
+    for i in range(3,len(dimensionsAndPixels)-1,3):
+        allPixels.append(Pixel(dimensionsAndPixels[i],dimensionsAndPixels[i+1],dimensionsAndPixels[i+2]))
+    return Image(length,width,allPixels)
+
+
+
+def saveImage(img,path):
+    f = open(path, "w")
+    f.write("P3\n")
+    f.write("#created by my wonderfull app !\n")
+    f.write(f'{img.length} {img.width} 255\n')
+    for i in range(3,len(img.pixels)-1,3):
+        f.write(img.pixels[i],img.pixels[i+1],img.pixels[i+2],"\n")
+
+
+

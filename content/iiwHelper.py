d00aef4cec3eb15b4e2f8fb423f2bc01bd46c0cf
julien deantoni <julien.deantoni@inria.fr>
2022-09-19 09:28:41 +0200

    minor changes

--- content/iiwHelper.py
+++ content/iiwHelper.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plotLib
 
 
-#This function takes a list of Point2D as parameter and plot them by using the matplotlib module
-def display2DPoints(lstPoint: list):
-    xSet: list =[]
-    ySet: list = []
-    for p in lstPoint:
-        xSet = xSet + [p.x]
-        ySet = ySet + [p.y]    
-    plotLib.plot(xSet, ySet, marker='.',  color = "red")
-    plotLib.show()
+def display2DPoints(*lstPoints: list):
+    '''This function takes a list of Point2D as parameter and plot them by using the matplotlib module'''
+    for lstPoint in lstPoints:
+        xSet: list =[]
+        ySet: list = []
+        for p in lstPoint:
+            xSet = xSet + [p.x]
+            ySet = ySet + [p.y]    
+        plt.plot(xSet, ySet, marker='.')
+    plt.show()
